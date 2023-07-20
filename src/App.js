ec283a2de8a7c21ec613d28ed4114410a49993af
Valeriya Medvedeva <frgt.pld@gmail.com>
2023-07-20 14:16:26 +0300

    added new add word screen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!!! My name is Lera!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 2,
     alignItems: 'center',
+    backgroundColor: '#9a0404',
     justifyContent: 'center',
   },
 });
